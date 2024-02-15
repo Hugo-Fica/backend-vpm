9da26258d3df3fb9abcbee9bf0ba724f9f9a4be7
Hugo-Fica <hugo.fica.saez@gmail.com>
2024-02-15 12:07:35 -0300

    rutas para edit modal operational streets

--- src/controllers/vector.controllers.js
+++ src/controllers/vector.controllers.js
@@ -1,15 +1,15 @@
-import { response, request } from 'express';
-import { Vector } from '../models/vector.models.js';
-import { User } from '../models/user.models.js';
-import { Area } from '../models/area.models.js';
-import { SubArea } from '../models/subArea.models.js';
-import { Activity } from '../models/activity.models.js';
-import { Criteria } from '../models/criteria.models.js';
-import { vectorForm } from '../helpers/transformData.js';
-import { ValueVector } from '../models/valueVector.models.js';
-import { Operational_Streets } from '../models/operational_streets.models.js';
-import { Equip_Vector_Value } from '../models/equip_vector_value.models.js';
-import { Operational_Streets_Values } from '../models/operational_streets_values.models.js';
+import { response, request } from 'express'
+import { Vector } from '../models/vector.models.js'
+import { User } from '../models/user.models.js'
+import { Area } from '../models/area.models.js'
+import { SubArea } from '../models/subArea.models.js'
+import { Activity } from '../models/activity.models.js'
+import { Criteria } from '../models/criteria.models.js'
+import { vectorForm } from '../helpers/transformData.js'
+import { ValueVector } from '../models/valueVector.models.js'
+import { Operational_Streets } from '../models/operational_streets.models.js'
+import { Equip_Vector_Value } from '../models/equip_vector_value.models.js'
+import { Operational_Streets_Values } from '../models/operational_streets_values.models.js'
 
 export const vectorGet = async (req = request, res = response) => {
   const vectors = await Vector.findAll({
@@ -21,24 +21,24 @@ export const vectorGet = async (req = request, res = response) => {
       { model: Criteria, attributes: ['name'] },
       {
         model: ValueVector,
-        attributes: ['id', 'position', 'value', 'period'],
-      },
+        attributes: ['id', 'position', 'value', 'period']
+      }
     ],
     attributes: {
-      exclude: ['user_id'],
-    },
-  });
+      exclude: ['user_id']
+    }
+  })
   if (vectors.length !== 0) {
-    const { newVectorFormArray } = vectorForm(vectors, {});
-    res.status(200).json(newVectorFormArray);
+    const { newVectorFormArray } = vectorForm(vectors, {})
+    res.status(200).json(newVectorFormArray)
   } else {
     res.status(200).json({
-      msg: 'no data in DB',
-    });
+      msg: 'no data in DB'
+    })
   }
-};
+}
 export const vectorGetById = async (req = request, res = response) => {
-  const { id } = req.params;
+  const { id } = req.params
   const vector = await Vector.findByPk(id, {
     include: [
       { model: User, attributes: ['user_name'] },
@@ -48,8 +48,8 @@ export const vectorGetById = async (req = request, res = response) => {
       { model: Criteria, attributes: ['name'] },
       {
         model: ValueVector,
-        attributes: ['id', 'position', 'value'],
-      },
+        attributes: ['id', 'position', 'value']
+      }
     ],
     attributes: {
       exclude: [
@@ -57,19 +57,43 @@ export const vectorGetById = async (req = request, res = response) => {
         'area_id',
         'sub_area_id',
         'activity_id',
-        'criteria_id',
-      ],
-    },
-  });
-  const { newVectorFormObj } = vectorForm([], vector);
+        'criteria_id'
+      ]
+    }
+  })
+  const { newVectorFormObj } = vectorForm([], vector)
   if (vector.length !== 0) {
-    res.status(200).json(newVectorFormObj);
+    res.status(200).json(newVectorFormObj)
   } else {
     res.status(200).json({
-      msg: 'no data in DB',
-    });
+      msg: 'no data in DB'
+    })
   }
-};
+}
+export const getOperationalStreet = async (req = request, res = response) => {
+  const id = req.params.id
+  const equip_vector_value = await Equip_Vector_Value.findAll({
+    where: { vector_id: id },
+    attributes: { exclude: ['id', 'vector_id'] }
+  })
+  const operational_streets = await Operational_Streets.findAll({
+    where: { vector_id: id }
+  })
+  const operational_streets_values = await Operational_Streets_Values.findAll({
+    where: { vector_id: id },
+    attributes: { exclude: ['id', 'vector_id'] }
+  })
+  const data = {
+    equipVectorValue: equip_vector_value
+      .sort((a, b) => a.value_x - b.value_x)
+      .map((opv) => ({ x: opv.value_x, y: opv.value_y })),
+    operationalStreets: operational_streets[0],
+    operationalStreetsValues: operational_streets_values
+      .sort((a, b) => a.value_x - b.value_x)
+      .map((opv) => ({ x: opv.value_x, y: opv.value_y }))
+  }
+  res.status(200).json(data)
+}
 export const vectorPost = async (req = request, res = response) => {
   const {
     user_id,
@@ -89,8 +113,8 @@ export const vectorPost = async (req = request, res = response) => {
     volume_m3,
     vector,
     position,
-    type_vector,
-  } = req.body;
+    type_vector
+  } = req.body
   const newVector = {
     user_id: user_id,
     area_id: area_id,
@@ -109,79 +133,79 @@ export const vectorPost = async (req = request, res = response) => {
     vector: vector,
     power_input: power_input,
     type_vector: type_vector,
-    position: position,
-  };
-  const addVector = Vector.build(newVector);
-  await addVector.save();
+    position: position
+  }
+  const addVector = Vector.build(newVector)
+  await addVector.save()
   res.status(201).json({
     msg: 'vector created correctly',
-    id: addVector.id,
-  });
-};
+    id: addVector.id
+  })
+}
 export const operational_street_post = async (
   req = request,
-  res = response,
+  res = response
 ) => {
   const { criteria_id, area_m2_2, air_velocity_2, fix_q_2, power_input_2 } =
-    req.body;
-  const vector_id = req.params.vector_id;
+    req.body
+  const vector_id = req.params.vector_id
   const addOP = {
     vector_id: vector_id,
     criteria_id: criteria_id,
     power_input_2: power_input_2,
     air_velocity_2: air_velocity_2,
     area_m2_2: area_m2_2,
-    fix_q_2: fix_q_2,
-  };
-  const addedOP = Operational_Streets.build(addOP);
-  await addedOP.save();
-  res.status(201).json({ msg: 'operational street created correctly' });
-};
+    fix_q_2: fix_q_2
+  }
+  const addedOP = Operational_Streets.build(addOP)
+  await addedOP.save()
+  res.status(201).json({ msg: 'operational street created correctly' })
+}
 
 export const equip_vector_value_post = async (
   req = request,
-  res = response,
+  res = response
 ) => {
-  const { vector_id, value_y, value_x } = req.body;
+  const { vector_id, value_y, value_x } = req.body
   const addEVV = {
     vector_id: vector_id,
     value_y: value_y,
-    value_x: value_x,
-  };
-  const addedEVV = Equip_Vector_Value.build(addEVV);
-  await addedEVV.save();
-  res.status(201).json({ msg: 'equip vector value created correctly' });
-};
+    value_x: value_x
+  }
+  const addedEVV = Equip_Vector_Value.build(addEVV)
+  await addedEVV.save()
+  res.status(201).json({ msg: 'equip vector value created correctly' })
+}
 
 export const operational_street_value_post = async (
   req = request,
-  res = response,
+  res = response
 ) => {
-  const { vector_id, value_y, value_x } = req.body;
+  const { vector_id, value_y, value_x } = req.body
   const addOSV = {
     vector_id: vector_id,
     value_y: value_y,
-    value_x: value_x,
-  };
-  const addedOSV = Operational_Streets_Values.build(addOSV);
-  await addedOSV.save();
-  res.status(201).json({ msg: 'operational street value created correctly' });
-};
-export const vectorPut = async (req = request, res = response) => {
-  const uuid = req.params.id;
-  const { id, vector, ...resto } = req.body;
-  console.log(resto);
-  const upVector = await Vector.findByPk(uuid);
-  const { vector: oldName } = upVector;
-  if (vector !== oldName) {
-    resto.vector = vector;
+    value_x: value_x
   }
-  await upVector.update(resto);
-  res.status(200).json({ msg: 'update vector' });
-};
+  const addedOSV = Operational_Streets_Values.build(addOSV)
+  await addedOSV.save()
+  res.status(201).json({ msg: 'operational street value created correctly' })
+}
+export const vectorPut = async (req = request, res = response) => {
+  const uuid = req.params.id
+  const { id, vector, ...resto } = req.body
+  console.log(resto)
+  const upVector = await Vector.findByPk(uuid)
+  const { vector: oldName } = upVector
+  if (vector !== oldName) {
+    resto.vector = vector
+  }
+  await upVector.update(resto)
+  res.status(200).json({ msg: 'update vector' })
+}
 export const vectorDelete = async (req = request, res = response) => {
-  const uuid = req.params.id;
-  const deleteVector = await Vector.findByPk(uuid);
+  const uuid = req.params.id
+  const deleteVector = await Vector.findByPk(uuid)
   await deleteVector.destroy({
     where: { id: uuid },
     cascade: true,
@@ -189,8 +213,8 @@ export const vectorDelete = async (req = request, res = response) => {
       ValueVector,
       Operational_Streets,
       Operational_Streets_Values,
-      Equip_Vector_Value,
-    ],
-  });
-  res.status(200).json({ msg: 'vector deleted' });
-};
+      Equip_Vector_Value
+    ]
+  })
+  res.status(200).json({ msg: 'vector deleted' })
+}
--- src/routes/vector.routes.js
+++ src/routes/vector.routes.js
@@ -1,26 +1,28 @@
-import { Router } from 'express';
+import { Router } from 'express'
 import {
   equip_vector_value_post,
+  getOperationalStreet,
   operational_street_post,
   operational_street_value_post,
   vectorDelete,
   vectorGet,
   vectorGetById,
   vectorPost,
-  vectorPut,
-} from '../controllers/vector.controllers.js';
-import { noVector, validatorVector } from '../helpers/db-validators.js';
-import { check } from 'express-validator';
-import { fieldValidation } from '../middlewares/field-validation.js';
+  vectorPut
+} from '../controllers/vector.controllers.js'
+import { noVector, validatorVector } from '../helpers/db-validators.js'
+import { check } from 'express-validator'
+import { fieldValidation } from '../middlewares/field-validation.js'
 
-export const vectorRt = Router();
+export const vectorRt = Router()
 
-vectorRt.get('/vectors', vectorGet);
+vectorRt.get('/vectors', vectorGet)
 vectorRt.get(
   '/:id',
   [check('id').custom(noVector), fieldValidation],
-  vectorGetById,
-);
+  vectorGetById
+)
+vectorRt.get('/operational/:id', getOperationalStreet)
 vectorRt.post(
   '/newVector',
   [
@@ -31,24 +33,24 @@ vectorRt.post(
     check('activity_id', 'activity is requerid').not().isEmpty(),
     check('criteria_id', 'criteria is requerid').not().isEmpty(),
     check('vector').custom(validatorVector),
-    fieldValidation,
+    fieldValidation
   ],
-  vectorPost,
-);
-vectorRt.post('/newOp/:vector_id', fieldValidation, operational_street_post);
-vectorRt.post('/newEVV', fieldValidation, equip_vector_value_post);
-vectorRt.post('/newOSV', fieldValidation, operational_street_value_post);
+  vectorPost
+)
+vectorRt.post('/newOp/:vector_id', fieldValidation, operational_street_post)
+vectorRt.post('/newEVV', fieldValidation, equip_vector_value_post)
+vectorRt.post('/newOSV', fieldValidation, operational_street_value_post)
 vectorRt.put(
   '/:id',
   [
     check('id').custom(noVector),
     check('vector').custom((value, { req }) => validatorVector(value, { req })),
-    fieldValidation,
+    fieldValidation
   ],
-  vectorPut,
-);
+  vectorPut
+)
 vectorRt.delete(
   '/:id',
   [check('id').custom(noVector), fieldValidation],
-  vectorDelete,
-);
+  vectorDelete
+)
